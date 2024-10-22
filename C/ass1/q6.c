e5547ef520278dcd108377e768786a5f8c70be48
Pushkar-12-design <pushkarkumar2630@gmail.com>
2024-10-23 00:54:06 +0530

    c ass1

---
+++ C/ass1/q6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+double celcius_at_depth(double);
+double farenheit(double);
+
+void main() {
+   double depth, celcius, frhen;
+   printf("Enter depth(in kilometers): ");
+   scanf("%lf", &depth);
+   celcius = celcius_at_depth(depth);
+   frhen = farenheit(celcius);
+   printf("Temperature at depth of %lf km is %lf℃ and %lf℉", depth, celcius, frhen);
+}
+
+double celcius_at_depth(double depth) {
+   return 10 * depth + 20;
+}
+
+double farenheit(double celcius) {
+   return 1.8 * celcius + 32;
+}
