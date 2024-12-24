9e5436b033c9f80902b2e9473827af83d029eb8f
Pushkar-12-design <pushkarkumar2630@gmail.com>
2024-12-24 18:04:41 +0530

    delete react

--- projectC/majorproject/q2.c
+++ projectC/majorproject/q2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int within_x_percent(double ref, double data, double x) {
     double lower_bound = ref - (x / 100.0) * ref;
@@ -7,21 +8,24 @@ int within_x_percent(double ref, double data, double x) {
 }
 
 int main() {
+    double boiling_points[] = {100.0, 357.0, 1187.0, 2193.0, 2660.0};
+    const char *substances[] = {"Water", "Mercury", "Copper", "Silver", "Gold"};
+    int num_substances = sizeof(boiling_points) / sizeof(boiling_points[0]);
     double observed_boiling_point;
-    printf("Enter the observed boiling point (C): ");
+    int found = 0;
+
+    printf("Enter the observed boiling point (°C): ");
     scanf("%lf", &observed_boiling_point);
 
-    if (within_x_percent(100, observed_boiling_point, 5)) {
-        printf("Substance: Water\n");
-    } else if (within_x_percent(357, observed_boiling_point, 5)) {
-        printf("Substance: Mercury\n");
-    } else if (within_x_percent(1187, observed_boiling_point, 5)) {
-        printf("Substance: Copper\n");
-    } else if (within_x_percent(2193, observed_boiling_point, 5)) {
-        printf("Substance: Silver\n");
-    } else if (within_x_percent(2660, observed_boiling_point, 5)) {
-        printf("Substance: Gold\n");
-    } else {
+    for (int i = 0; i < num_substances; i++) {
+        if (within_x_percent(boiling_points[i], observed_boiling_point, 5.0)) {
+            printf("The substance is %s\n", substances[i]);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
         printf("Substance Unknown\n");
     }
 
