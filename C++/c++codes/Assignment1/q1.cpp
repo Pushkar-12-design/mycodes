4a150311b2078b816d818d66ede23c116e785351
Pushkar-12-design <pushkarkumar2630@gmail.com>
2025-04-08 01:19:43 +0530

    updated

--- C++/c++codes/Assignment1/q1.cpp
+++ C++/c++codes/Assignment1/q1.cpp
@@ -40,26 +40,3 @@ int main ()
         return 0;
 }
 
-// #include <iostream>
-// using namespace std;
-
-// enum Day { SUNDAY = 1, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY };
-
-// int main() {
-//     int dayNumber;
-//     cout << "Enter a day number (1-7): ";
-//     cin >> dayNumber;
-
-//     switch (dayNumber) {
-//         case SUNDAY: cout << "The day is Sunday.\n"; break;
-//         case MONDAY: cout << "The day is Monday.\n"; break;
-//         case TUESDAY: cout << "The day is Tuesday.\n"; break;
-//         case WEDNESDAY: cout << "The day is Wednesday.\n"; break;
-//         case THURSDAY: cout << "The day is Thursday.\n"; break;
-//         case FRIDAY: cout << "The day is Friday.\n"; break;
-//         case SATURDAY: cout << "The day is Saturday.\n"; break;
-//         default: cout << "Invalid day number!\n";
-//     }
-
-//     return 0;
-// }
